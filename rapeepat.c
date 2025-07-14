ae33d470d1e97439baeaf100d604a52a3fb0327f
0951692659aS <s6812022610280@email.kmutnb.ac.th>
2025-07-14 16:53:06 +0700

    1

---
+++ rapeepat.c
@@ -0,0 +1,8 @@
+#include<stdio.h> //#include: preprocessor Directive
+
+int main (){ // int: keyword, min; identifier, (); Operator
+   printf("Hello,\n"); // printf; identifier(); Operator,"Hello,\n"; constant
+   printf("C Tokens!\n"); // printf; identifier(); Operator,"C tokens!\n"; constant
+   return 0; //return; keyword, 0;constant, :; Operator
+
+}
